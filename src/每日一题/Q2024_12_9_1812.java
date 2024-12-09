73b4b3b34b339075aed4913e7b8b87e3696567b4
jzab-nb <1970717299@qq.com>
2024-12-09 19:06:03 +0800

    2024年12月9日 每日一题

---
+++ src/每日一题/Q2024_12_9_1812.java
@@ -0,0 +1,12 @@
+package 每日一题;
+
+/**
+ * @author JZAB
+ */
+public class Q2024_12_9_1812 {
+    public boolean squareIsWhite(String coordinates) {
+        int i = coordinates.charAt(0)-'a';
+        int j = coordinates.charAt(1)-1;
+        return i%2!=j%2;
+    }
+}
