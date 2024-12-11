a0ccba044bcc5a8022b00fb9d0818731bae28818
jzab-nb <1970717299@qq.com>
2024-12-11 13:13:00 +0800

    2024/12/11 每日一题

--- src/周赛/C427.java
+++ src/周赛/C427.java
@@ -1,10 +1,14 @@
 package 周赛;
 
+import java.util.Arrays;
+
 /**
  * @author JZAB
  */
 public class C427 {
-
+    public static void main(String[] args) {
+        new Solution3().maxSubarraySum(new int[]{-1,-2,-3,-4,-5},4);
+    }
 }
 
 class Solution1 {
@@ -37,16 +41,21 @@ class Solution3 {
         long[] qzh = new long[n+1];
         // 先计算前缀和
         for(int i=1;i<=n;i++) qzh[i]+=qzh[i-1]+nums[i-1];
-//        System.out.println("   "+Arrays.toString(nums) );
-//        System.out.println(Arrays.toString(qzh) );
+        System.out.println("   "+ Arrays.toString(nums) );
+        System.out.println(Arrays.toString(qzh) );
+        // 记录对k取余的不同数的最小值
+        long[] minS = new long[k];
+        Arrays.fill(minS,Long.MAX_VALUE/2);
         long max=Long.MIN_VALUE;
-        // 遍历数组长度从1k,2k,3k
-        for(int len=k;len<=n;len+=k){
-            for(int i=0;i+len<n+1;i++){
-//                System.out.printf("%d %d\n",i,i+len );
-                max = Math.max(max,-qzh[i]+qzh[i+len]);
-            }
+        for(int j=0;j<qzh.length;j++){
+            // j对k取余,等于i,前面的对k取余也等于i的就是要减去的下标
+            int i = j%k;
+            // 答案取最大值
+            max = Math.max(max,qzh[j]-minS[i]);
+            // 出现在j之前的对k取余等于i的最小值
+            minS[i] = Math.min(minS[i],qzh[j]);
         }
+
         return max;
     }
 }
---
+++ src/每日一题/Q2024_12_11_2717.java
@@ -0,0 +1,16 @@
+package 每日一题;
+
+/**
+ * @author JZAB
+ */
+public class Q2024_12_11_2717 {
+    public int semiOrderedPermutation(int[] nums) {
+        int n = nums.length;
+        int l=0,r=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1) l = i;
+            if(nums[i]==n) r = i;
+        }
+        return l+(n-r-1)-(l>r?1:0);
+    }
+}
