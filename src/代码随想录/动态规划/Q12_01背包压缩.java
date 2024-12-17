c05fa58cf88e2445db244c793a65ac2493c76663
jzab <1970717299@qq.com>
2024-12-17 20:30:06 +0800

    提交

--- src/代码随想录/动态规划/Q12_01背包压缩.java
+++ src/代码随想录/动态规划/Q12_01背包压缩.java
@@ -1,9 +1,8 @@
 package 代码随想录.动态规划;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class Q11_01背包 {
+public class Q12_01背包压缩 {
     public static void main(String[] args) {
         //第一行包含两个正整数，第一个整数 M 代表研究材料的种类，第二个正整数 N，代表小明的行李空间。
         //
@@ -18,24 +17,24 @@ public class Q11_01背包 {
         for(int i=0;i<n;i++) space[i] = in.nextInt();
         for(int i=0;i<n;i++) value[i] = in.nextInt();
         // dp[i][j]代码背包容量为j时且能选择前i个物品时，能获得的最大价值
-        int[][] dp = new int[n][maxValue+1];
+        int[] dp = new int[maxValue+1];
         for(int i=0;i<=maxValue;i++){
             // 初始化第一行
-            if(space[0]<=i) dp[0][i] = value[0];
+            if(space[0]<=i) dp[i] = value[0];
         }
         for(int i=1;i<n;i++){
-            for(int j=0;j<=maxValue;j++){
+            for(int j=maxValue;j>=0;j--){
                 // 不放第i个物品
-                dp[i][j] = dp[i-1][j];
+                dp[j] = dp[j];
                 // 假如能放第i个物品
                 if(j>=space[i]){
-                    dp[i][j] = Math.max(dp[i][j],dp[i-1][j-space[i]]+value[i]);
+                    dp[j] = Math.max(dp[j],dp[j-space[i]]+value[i]);
                 }
             }
         }
 //        System.out.println(Arrays.toString(space));
 //        System.out.println(Arrays.toString(value));
 //        System.out.println(Arrays.deepToString(dp));
-        System.out.println(dp[n-1][maxValue]);
+        System.out.println(dp[maxValue]);
     }
 }
