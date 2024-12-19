f34a5a7bad51461bcd6c6db333ff56817c220686
jzab <1970717299@qq.com>
2024-12-19 12:28:47 +0800

    2024/12/19 每日一题

---
+++ src/每日一题/Q2024_12_19_3285.java
@@ -0,0 +1,16 @@
+package 每日一题;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q2024_12_19_3285 {
+    public List<Integer> stableMountains(int[] height, int threshold) {
+        List<Integer> result = new ArrayList<>();
+        for(int i=1;i<height.length;i++){
+            if(height[i-1]>threshold){
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
