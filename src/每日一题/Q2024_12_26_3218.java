39ada5f7db5c6de2a9b1d7487c7b248ce1c587d6
jzab <1970717299@qq.com>
2024-12-26 18:04:32 +0800

    2024/12/26 每日一题

---
+++ src/每日一题/Q2024_12_26_3218.java
@@ -0,0 +1,20 @@
+package 每日一题;
+
+public class Q2024_12_26_3218 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSubstringPresent(String s) {
+        // 记录两个相邻字符出现的情况
+        boolean[][] map = new boolean[26][26];
+        for(int i=0;i<s.length()-1;i++){
+            int a = s.charAt(i)-'a';
+            int b = s.charAt(i+1)-'a';
+            map[a][b] = true;
+            // 如果反向的存在或者两个字符相等，则返回true
+            if(map[b][a] || a==b) return true;
+        }
+        return false;
+    }
+}
