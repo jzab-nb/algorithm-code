ebb77700392f0da1819737462e6fa267fa70753f
jzab <1970717299@qq.com>
2024-12-28 13:24:00 +0800

    2024/12/28 每日一题

---
+++ src/每日一题/Q2024_12_28_3046.java
@@ -0,0 +1,17 @@
+package 每日一题;
+
+public class Q2024_12_28_3046 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isPossibleToSplit(int[] nums) {
+        // 出现次数不能大于2
+        int[] mem = new int[101];
+        for(int i:nums){
+            mem[i]++;
+            if(mem[i]>=3) return false;
+        }
+        return true;
+    }
+}
