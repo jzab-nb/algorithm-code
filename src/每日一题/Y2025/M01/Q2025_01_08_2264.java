e57e5796776a521ccb7c839898f4a5f64323179f
jzab <1970717299@qq.com>
2025-01-08 11:19:08 +0800

    2024/01/08 每日一题

---
+++ src/每日一题/Y2025/M01/Q2025_01_08_2264.java
@@ -0,0 +1,17 @@
+package 每日一题.Y2025.M01;
+
+public class Q2025_01_08_2264 {
+
+    public String largestGoodInteger(String num) {
+        int l=0,m=1,r=2;
+        int res=0;
+        while(r<num.length()){
+            if(num.charAt(l)==num.charAt(m) && num.charAt(m)==num.charAt(r)){
+                res = Math.max((int)num.charAt(l),res);
+            }
+            l++;m++;r++;
+        }
+        if(res==0) return "";
+        return new String(new char[]{(char)res,(char)res,(char)res});
+    }
+}
